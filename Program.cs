d17b5b74b0299f829bc68ff0f7c2a912e838cce3
josie <josephine.lucyrita@gmail.com>
2024-11-16 19:36:05 +0000

    adds Program, Employee and OfficeManager Classes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, Scamperers");
+﻿using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+
+    public Employee(string name)
+    {
+        FirstName = name;
+    }
+}
+
+public class OfficeManager
+{
+    public void WelcomeNewEmployee(List<Employee> newEmployees)
+    {
+        foreach (var employee in newEmployees)
+        {
+            Console.WriteLine($"Hello, {employee.FirstName}. Welcome to the company, you will have lots of fun here!");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<Employee> newEmployees = new List<Employee>
+        {
+            new Employee("Ross"),
+            new Employee("Rachel"),
+            new Employee("Monica"),
+            new Employee("Phoebe"),
+            new Employee("Joey"),
+            new Employee("Chandler")
+        };
+
+        OfficeManager gunther = new OfficeManager();
+        gunther.WelcomeNewEmployee(newEmployees);
+    }
+}
--- obj/Debug/net8.0/ScamperersConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScamperersConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScamperersConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d9ea1303d272e74c8062400e27c7165ca5bb14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScamperersConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScamperersConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
