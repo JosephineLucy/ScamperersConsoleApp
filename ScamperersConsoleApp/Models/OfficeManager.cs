2a2b0a9e843acea41a759ca41363e585c6e69167
josie <josephine.lucyrita@gmail.com>
2024-12-08 16:27:10 +0000

    adds InterviewCandidate method to OfficeManager

--- ScamperersConsoleApp/Models/OfficeManager.cs
+++ ScamperersConsoleApp/Models/OfficeManager.cs
@@ -2,6 +2,12 @@
 {
     public class OfficeManager
     {
+        public void InterviewCandidate()
+        {
+            Console.WriteLine("Enter the candidate name you would like to interview:");
+            string candidateName = Console.ReadLine();
+            Console.WriteLine($"You have chosen to interview {candidateName}");
+        }
         public void WelcomeNewEmployee(List<Employee> newEmployees)
         {
             foreach (var employee in newEmployees)
